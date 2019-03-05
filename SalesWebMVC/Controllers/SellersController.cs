c788b4416acfaff66d8f7a5975e3195fbd0e8a1e
Luiz Felipe <raziel_libertino@hotmail.com>
2019-03-04 23:04:10 -0400

    Seller details and eager loading

--- SalesWebMVC/Controllers/SellersController.cs
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -65,5 +65,21 @@ namespace SalesWebMVC.Controllers
             _sellerServices.Remove(id);
             return RedirectToAction(nameof(Index));
         }
+
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerServices.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
     }
 }
